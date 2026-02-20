8eb3525113b990565bbf10c23c7c7d912332b688
dhanyaca99 <dhanyaca99@users.noreply.github.com>
2026-02-20 23:19:15 +0530

    Save workspace changes

---
+++ Chapter_03_Identifier_literal_operator_statements/10_identifier.js
@@ -0,0 +1,8 @@
+   //Template literal
+   let name = "John";
+   let full_name = `${name} Doe`;
+   console.log(full_name);
+
+   //path
+   let path = "C:\\Users\\Dhanya\\Documents\\LearningPlaywright";
+   console.log(path);
---
+++ Chapter_03_Identifier_literal_operator_statements/11_null_type.js
@@ -0,0 +1,9 @@
+//null - intentionally nothing
+
+let selected_item = null;
+console.log(selected_item);
+
+//undefined - not yet assigned a value
+
+let username;
+console.log(username); 
